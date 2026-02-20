d37440a51b24d0296a922c4cff676dab229e6c41
kajalti@29 <kajaltisirame@gmail.com>
2026-02-20 14:07:25 +0530

    code commit

---
+++ 01_JS Code/03_Mycodes/D_Numerical code/07_Duplicate.js
@@ -0,0 +1,11 @@
+// let arr = [1,2,4,5,3,2,3,4]
+// let unique  = [];
+
+// for(let i = 0; i< arr.length; i++){
+//     if(!unique.includes(arr[i])){
+//         unique.push(arr[i]);
+//     }
+// }
+
+// console.log(unique);
+
