ec9068dc5cc5785bbe947deafb39bcc8930a4d89
kajalti@29 <kajaltisirame@gmail.com>
2026-02-21 13:29:53 +0530

    code commit

---
+++ 02_Small Code/05_logicalCode/05_frequency.js
@@ -0,0 +1,9 @@
+function frequency(arr){
+    let obj = {};
+
+    for(let num of arr){
+        obj[num] = (obj[num] || 0) + 1;
+    }
+    return obj;
+}
+console.log(frequency([1, 2, 2, 3, 3, 3]));
