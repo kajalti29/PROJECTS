32f28ac9ef792be7089076dde659f8677b491688
kajalti@29 <kajaltisirame@gmail.com>
2026-02-22 01:16:46 +0530

    code commit

---
+++ 09_ShowHidePassword/script.js
@@ -0,0 +1,11 @@
+let password = document.getElementById("password");
+let toggle = document.getElementById("toggle");
+
+toggle.addEventListener("click", function() {
+    if(password.type === "password") {
+        password.type = "text";
+        toggle.textContent = "Hide ";
+    } else {
+        password.type = "password";
+        toggle.textContent = "Show";
+    }});     
