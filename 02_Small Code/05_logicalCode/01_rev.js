12a3c5b748ea20430a2ca1263d186070d2682adb
kajalti@29 <kajaltisirame@gmail.com>
2026-02-21 10:49:10 +0530

    code commit

---
+++ 02_Small Code/05_logicalCode/01_rev.js
@@ -0,0 +1,11 @@
+// Reverse a string without using inbuilt reverse()
+
+function reverseString(str){
+    let reversed = "";
+
+    for(let i = str.length-1; i>=0; i--){
+        reversed += str[i];
+    }
+    return reversed;
+}
+console.log(reverseString("kajalti"));
