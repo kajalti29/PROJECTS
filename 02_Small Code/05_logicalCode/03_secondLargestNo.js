7125fd3c7a1122f485cfe7213b7d3557a103d787
kajalti@29 <kajaltisirame@gmail.com>
2026-02-21 11:51:30 +0530

    code commit

---
+++ 02_Small Code/05_logicalCode/03_secondLargestNo.js
@@ -0,0 +1,17 @@
+// Find Second Largest Number (Very Important)
+
+function secondLargest(arr){
+    let largest = -Infinity;
+    let second = -Infinity;
+
+    for(let num of arr){
+        if(num > largest){
+            second = largest;
+            largest = num;
+        }else if (num > second && num !== largest){
+            second = num;
+        }
+    }
+    return second;
+}
+console.log(secondLargest([2, 4, 5, 2, 5, 6, 8]));
