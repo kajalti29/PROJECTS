574ca89dfbfc07a9627d8d473c08fa2edd5d6e19
kajalti@29 <kajaltisirame@gmail.com>
2026-03-07 20:15:28 +0530

    code commit

--- 02_Small Code/06_DOM/10_To-do List/script.js
+++ 02_Small Code/06_DOM/10_To-do List/script.js
@@ -1,21 +1,10 @@
-const input = document.getElementById('taskInput');
-const addBtn = document.getElementById('addTask');
-const list = document.getElementById('taskList');  
+function addTask(){
+    let input = document.getElementById("task")
+    let li = document.createElement("li")
 
-addBtn.addEventListener('click', function() {
-    if(input.value == "") return; // Prevent adding empty tasks
+    li.textContent = input.value
+ 
+    document.getElementById("list").appendChild(li)
 
-    const li = document.createElement('li');
-    li.textContent = input.value;
-
-    const deleteBtn = document.createElement('button');
-    deleteBtn.textContent = 'Delete';
-
-    deleteBtn.addEventListener('click', function() {
-        list.removeChild(li);
-    });
-
-    li.appendChild(deleteBtn);
-    list.appendChild(li);
-    input.value = ''; // Clear input after adding task
-} );
+    input.value = ""
+}
