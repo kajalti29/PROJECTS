f5d44ebbc5a0696e53dd3fde9c342cb8ef8e9039
kajalti@29 <kajaltisirame@gmail.com>
2026-05-06 19:22:40 +0530

    code commit

---
+++ 19_React/04_CounterApp/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { useState } from 'react'
+
+const App = () => {
+
+  const [count, setCount] = useState(0)
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button onClick={() => setCount(count + 1)}>Increase</button>
+      <button onClick={() => setCount(count - 1)}>Decrease</button>
+      <button onClick={() => setCount(0)}>Reset</button>
+
+    </div>
+  )
+}
+
+export default App
