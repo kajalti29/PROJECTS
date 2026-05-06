fab82466bacbd8e6867814eaf0f611ecc309e60d
kajalti@29 <kajaltisirame@gmail.com>
2026-05-06 17:35:18 +0530

    code commit

---
+++ 19_React/03_Props/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Child from './child';
+
+const App = () => {
+  return (
+    <div>
+      <h2>This is Parent(App)</h2>
+      <Child name="kajal"/> 
+    </div>
+  )
+}
+
+export default App
