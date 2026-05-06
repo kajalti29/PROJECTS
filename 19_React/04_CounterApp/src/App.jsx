5867a68efa6de095dc0006b5d8b97e44faee3c9a
kajalti@29 <kajaltisirame@gmail.com>
2026-05-06 19:24:09 +0530

    code commit

--- 19_React/04_CounterApp/src/App.jsx
+++ 19_React/04_CounterApp/src/App.jsx
@@ -10,9 +10,7 @@ const App = () => {
       <button onClick={() => setCount(count + 1)}>Increase</button>
       <button onClick={() => setCount(count - 1)}>Decrease</button>
       <button onClick={() => setCount(0)}>Reset</button>
-
     </div>
   )
 }
-
 export default App
